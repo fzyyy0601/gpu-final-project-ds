e948a3c78abba36ad2e197c0b49cc37bf8803831
liwei <liwei@10-16-142-208.dynapool.wireless.nyu.edu>
2023-12-01 18:02:42 -0500

    split task

--- coo.hxx
+++ coo.hxx
@@ -12,68 +12,99 @@ class coo{
 
 public:
     /*
+    1
+    */
+    void init(int vn,int en,int wn){
+
+    }
+    /*
+    2
     */
     int get_number_of_vertices() const {
 
         return 0;
     }
     /*
+    3
     */
     int get_number_of_edges(){
 
     }
     /*
-
+    4
     */
     int get_number_of_neighbors(vertex_type x){
 
     }
     /*
+    1
     */
     void set(){
 
     }
-
+    /*
+    2
+    */
     vertex_t get_source_vertex(){
 
     }
-
+    /*
+    3
+    */
     vertex_t get_destination_vertex(){
 
     }
-
+    /*
+    4
+    */
     edge_t get_edge(edge_t){
 
     }
-
+    /*
+    1
+    */
     vertex_t get_vertex(vertex_t){
 
     }
-
+    /*
+    2
+    */
     edge_t get_weight(edge_t){
 
     }
-
+    /*
+    3
+    */
     int get_in_degree(vertex_t){
 
     }
-
+    /*
+    4
+    */
     int get_out_degree(vertex_t){
 
     }
-
+    /*
+    1
+    */
     edge_t insert_edge(edge_t){
 
     }
-
+    /*
+    2
+    */
     vertex_t insert_vertex(edge_t){
 
     }
-
+    /*
+    3
+    */
     edge_t delete_edge(edge_t){
 
     }
-
+    /*
+    4
+    */
     vertex_t deete_vertex(vertex_t){
         
     }
