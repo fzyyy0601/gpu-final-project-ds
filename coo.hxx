b827612097c9dfe93d016121b6da69d8f96cbb83
liwei <liwei@10-16-142-208.dynapool.wireless.nyu.edu>
2023-12-01 17:20:36 -0500

    "commit"

--- coo.hxx
+++ coo.hxx
@@ -1,3 +1,70 @@
 class coo{
 
+public:        
+    /*
+    */
+    int get_number_of_vertices() const {
+
+        return 0;
+    }
+    /*
+    */
+    int get_number_of_edges(){
+
+    }
+    /*
+
+    */
+    int get_number_of_neighbors(vertex_type x){
+
+    }
+    /*
+    */
+    void set(){
+
+    }
+
+    vertex_t get_source_vertex(){
+
+    }
+
+    vertex_t get_destination_vertex(){
+
+    }
+
+    edge_t get_edge(edge_t){
+
+    }
+
+    vertex_t get_vertex(vertex_t){
+
+    }
+
+    edge_t get_weight(edge_t){
+
+    }
+
+    int get_in_degree(vertex_t){
+
+    }
+
+    int get_out_degree(vertex_t){
+
+    }
+
+    edge_t insert_edge(edge_t){
+
+    }
+
+    vertex_t insert_vertex(edge_t){
+
+    }
+
+    edge_t delete_edge(edge_t){
+
+    }
+
+    vertex_t deete_vertex(vertex_t){
+        
+    }
 };
--- graph.hxx
+++ graph.hxx
@@ -4,16 +4,21 @@
 
 #include <coo.hxx>
 
-template<class... graph_view_t> class graph {
+template<
+    typename vertex_t,
+    typename edge_t,
+    typename weight_t,
+    class... graph_view_t> class graph {
 
+public:
     graph_view_t graph_t;
 
-    __host__ __device__ __forceinline__
+    __host__ __device__
     get_number_of_vertices() const {
         return graph_t.get_number_of_vertices();
     }
 
-    __host__ __device__ __forceinline__
+    __host__ __device__
     get_number_of_edges() const {
         return graph_t.get_number_of_edges();
     }
