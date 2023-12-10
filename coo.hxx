5cfeac67b70d5b48b47ade7f290a825a0cae9194
Ziyue Feng <1359787436@qq.com>
2023-12-09 21:10:46 -0500

    update

--- coo.hxx
+++ coo.hxx
@@ -395,7 +395,7 @@ public:
         cudaMalloc(&cudaList, sizeof(size_t)* num);
         //cudaMalloc(&d_count, sizeof(int));
         //cudaMemcpy(d_count, &count, sizeof(int), cudaMemcpyHostToDevice);
-        get_destination_vertex_d<<<number_of_blocks, threads_per_block>>>(cudaList, x, col_idx_d, d_count, MAX_d);
+        get_destination_vertex_d<<<number_of_blocks, threads_per_block>>>(cudaList, x, col_idx_d, d_count, MAX_h);
         cudaMemcpy(list, cudaList, sizeof(size_t)* num, cudaMemcpyDeviceToHost);
         //cudaMemcpy(d_count, &count, sizeof(int), cudaMemcpyDeviceToHost);
         std::vector<size_t> ret(num);
@@ -415,7 +415,7 @@ public:
         cudaMalloc(&cudaList, sizeof(size_t)* num);
         //cudaMalloc(&d_count, sizeof(int));
         //cudaMemcpy(d_count, &count, sizeof(int), cudaMemcpyHostToDevice);
-        get_source_vertex_d<<<number_of_blocks, threads_per_block>>>(cudaList, x, row_idx_d, d_count, MAX_d);
+        get_source_vertex_d<<<number_of_blocks, threads_per_block>>>(cudaList, x, row_idx_d, d_count, MAX_h);
         cudaMemcpy(list, cudaList, sizeof(size_t)* num, cudaMemcpyDeviceToHost);
         //cudaMemcpy(d_count, &count, sizeof(int), cudaMemcpyDeviceToHost);
         std::vector<size_t> ret(num);
