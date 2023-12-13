ff22c54325819bd95cfd02ebe2ecd3c2e3936815
Baijia Ye <779062576@qq.com>
2023-12-13 00:18:41 -0500

    modify test.cu

--- test.cu
+++ test.cu
@@ -3,14 +3,6 @@
 #include <iostream>
 #include <vector>
 
-/* 
-This is the graph we test
-    insert nodes: 1 5 7 9 10
-    insert edge: (1,5) (7,9) (7,10)
-    vertex=[1,5,7,9,10]
-    row=[0,2,2]
-    col=[1,3,4]
-*/
 
 int main(){
     /* intital graph configuration */
